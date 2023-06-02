590f58c8d2316eb4df64cb7b372ca04489a89228
Theresa <theresa.dietinger@gmail.com>
2023-06-02 10:50:56 +0200

    update 3rd game

--- conceptional_game_3/Assets/Motion.cs
+++ conceptional_game_3/Assets/Motion.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Motion : MonoBehaviour
 {
 
+    public Material white;
+    public Material highlight;
+    private bool isReversedGravity = false;
     float getRandomValue()
     {
         return Random.Range(-5, 5);
@@ -13,10 +16,19 @@ public class Motion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        MeshRenderer colour = GetComponent<MeshRenderer>();
+
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Rigidbody rb = GetComponent<Rigidbody>();
             rb.velocity = new Vector3(getRandomValue(), getRandomValue(), getRandomValue());
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            colour.material = isReversedGravity ? white : highlight;
+            rb.useGravity= isReversedGravity;
+            isReversedGravity = !isReversedGravity;
         }
     }
 }
